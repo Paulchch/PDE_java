02d791de3d934c443426084d3c692531c4e7cc34
ESCINF <pablitombcr@gmail.com>
2022-09-02 11:28:37 -0600

    update

--- src/main/java/pruebas/ButtonMouseListener.java
+++ src/main/java/pruebas/ButtonMouseListener.java
@@ -1,11 +1,14 @@
 package pruebas;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 //extends MouseAdapter-->Clase tiene implementación de MouseListener pero vacío para no tener que hacer todos los métodos de dsdsf
 public class ButtonMouseListener extends MouseAdapter implements MouseListener {
+
+    Color c;
     @Override
         public void mouseClicked(MouseEvent e){
         //casting para la compilación, porque originalmente devuelve uno arriba de la jerarquía (Component object)
@@ -13,5 +16,19 @@ public class ButtonMouseListener extends MouseAdapter implements MouseListener {
         System.out.println("Click!!");
         System.out.println(b.getText());
     }
+
+    @Override
+    public void mouseEntered(MouseEvent e){
+        JButton b = (JButton) e.getSource();
+        b.setBackground(Color.green);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e){
+        JButton b = (JButton) e.getSource();
+        c = b.getBackground();
+        b.setBackground(c); //para que al salir sea del color que era en un principio
+    }
+
 }
 //swing->component->jradiobutton,jlabel,etc
--- src/main/java/pruebas/View.java
+++ src/main/java/pruebas/View.java
@@ -1,6 +1,8 @@
 package pruebas;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class View {
     private JButton agregar;
@@ -16,8 +18,22 @@ public class View {
 
     public View() {
         ButtonMouseListener bml = new ButtonMouseListener();
-        agregar.addMouseListener(bml);
-        borrar.addMouseListener(bml);
+        //agregar.addMouseListener(bml);
+        //borrar.addMouseListener(bml);
         //se deberia hacer agregarMouseListener y borrarMouseListener por aparte para que hagan cosas diferentes
+
+        //llave luego de new-->subclase de MouseAdapter
+        agregar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Agregar! New");
+            }
+        });
+        borrar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Borrar! New");
+            }
+        });
     }
 }
